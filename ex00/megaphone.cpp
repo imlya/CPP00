5df63fcad88e81a1810898ee621d32d022644b46
Atek Imene <Moon@MacBook-Air-de-Atek.local>
2025-05-06 00:08:36 +0200

    megaphone corrigé, phonebook pas au point

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: Moon <Moon@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/30 13:39:25 by imatek            #+#    #+#             */
-/*   Updated: 2025/05/03 12:29:23 by Moon             ###   ########.fr       */
+/*   Updated: 2025/05/06 00:07:32 by Moon             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,7 @@ int main(int ac, char **av)
     if (ac == 1)
         std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
     for (int i = 1; av[i]; i++)
-    {
         for (int j = 0; av[i][j] ; j++)
-        {
-			// est-ce possible d'utiliser std::locale.toupper ?
-            if (av[i][j] >= 'a' && av[i][j] <= 'z')
-                av[i][j] -= 32;
-            std::cout << av[i][j];
-        }
-    }
+            std::cout << (char)toupper(av[i][j]);
     std::cout << std::endl;
 }
